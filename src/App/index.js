b206d28363421b8f56ead06a8ddd130e8458435b
Jes Ababon <jesababon@gmail.com>
2018-07-16 14:53:52 -0400

    still trying to deploy. added npm and node versions

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route} from "react-router-dom";
-require('dotenv').config()
 import "./style.css";
 import Search from "../Search";
 // import TestArtsPage from "../TestArtsPage";
